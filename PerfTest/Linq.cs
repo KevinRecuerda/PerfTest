6baa5c222dc1d3a6a08722141ce94e24e9f72660
KevinRecuerda <kevin.recuerda@gmail.com>
2017-03-12 23:57:12 +0100

    PerfTest solution & Linq comparison

---
+++ PerfTest/Linq.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PerfTest
+{
+    public class Linq
+    {
+        private List<Item1> items;
+
+        [Setup]
+        public void SetUp()
+        {
+            this.items = CreateItems(this.RowCount, this.ColumnCount);
+        }
+
+        private static List<Item1> CreateItems(int rowCount, int columnCount)
+        {
+            return Enumerable
+                .Range(1, rowCount)
+                .Select(x => new Item1()
+                {
+                    Id = $"Id{x}",
+                    KeyValues = Enumerable.Range(1, columnCount)
+                                          .Select(y => new KeyValuePair<string, double>($"Key{y}", (x-1)*columnCount + y))
+                                          .ToList()
+                })
+                .ToList();
+        }
+
+        [Params(100, 1000, 10000)]
+        public int RowCount { get; set; }
+
+        [Params(10, 100, 1000)]
+        public int ColumnCount { get; set; }
+
+        [Benchmark(Description = "Linq")]
+        public Dictionary<string, List<double>> GroupBy()
+        {
+            return this.items.SelectMany(x => x.KeyValues)
+                             .GroupBy(x => x.Key)
+                             .ToDictionary(x => x.Key, x => x.Select(y => y.Value).ToList());
+        }
+
+        [Benchmark(Description = "Basic")]
+        public Dictionary<string, List<double>> Basic()
+        {
+            var result = new Dictionary<string, List<double>>();
+            foreach (var item in this.items)
+            {
+                foreach (var keyValuePair in item.KeyValues)
+                {
+                    if (!result.ContainsKey(keyValuePair.Key))
+                        result[keyValuePair.Key] = new List<double>();
+
+                    result[keyValuePair.Key].Add(keyValuePair.Value);
+                }
+            }
+            return result;
+        }
+    }
+
+    public class Item1
+    {
+        public string Id { get; set; }
+        public List<KeyValuePair<string, double>> KeyValues { get; set; }
+    }
+}
